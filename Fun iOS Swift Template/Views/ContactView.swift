6f4f553b353e2f1b85e89b9ebfc6089fe4be9551
Isaac <codejourneysoftware@gmail.com>
2025-09-23 01:44:58 -0700

    Not sure.

--- Fun iOS Swift Template/Views/ContactView.swift
+++ Fun iOS Swift Template/Views/ContactView.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
-struct NotesView: View {
+struct ContactView: View {
     var body: some View {
         VStack {
-            Image(systemName: "note.text")
+            Image(systemName: "person.crop.circle")
                 .resizable()
                 .frame(width: 80, height: 80)
                 .foregroundColor(.accentColor)
-            Text("Notes")
+            Text("Contacts")
                 .font(.largeTitle)
                 .bold()
-            Text("This is the Notes tab.")
+            Text("This is the Contacts tab.")
                 .foregroundStyle(.secondary)
         }
         .padding()
@@ -18,5 +18,5 @@ struct NotesView: View {
 }
 
 #Preview {
-    NotesView()
+    ContactView()
 }
